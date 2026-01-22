d24832090967cd34d3fc969095821ab8f02b82c8
aaares <aaares1@gmail.com>
2026-01-22 23:18:57 +0100

    test fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,11 @@
 import unittest
-from src.app import app
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from app import app
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
